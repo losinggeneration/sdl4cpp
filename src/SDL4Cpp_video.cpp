losinggeneration/sdl4cpp

commit dc6d813f4398119da27520c078b95cc05a43654a

Enabled Effective C++ warnings
* Enabled and changed some code to comply with the Effective C++
* warnings. Still haven't got it all cleaned up with the most notable
* part being SDL::Rect inheriting a sturct. As of yet I haven't thought
* of a good solution to this minor problem.

Signed-off-by: Harley Laue <losinggeneration@gmail.com>

src/SDL4Cpp_video.cpp
@@ -71,7 +71,6 @@ namespace SDL
 
 	Rect::Rect()
 	{
-		x = y = h = w = 0;
 	}
 
 	Rect::Rect(const Rect &rect)
@@ -81,10 +80,10 @@ namespace SDL
 
 	Rect::Rect(int x, int y, int w, int h)
 	{
-		this->x = x;
-		this->y = y;
-		this->w = w;
-		this->h = h;
+		x = x;
+		y = y;
+		w = w;
+		h = h;
 	}
 
 	Rect::Rect(const SDL_Rect &rect)
@@ -98,8 +97,8 @@ namespace SDL
 
 	bool Rect::operator ==(const SDL_Rect &rect)
 	{
-		 return (x == rect.x) && (y == rect.y) && (w == rect.w) &&
-			                         (h == rect.h);
+		 return (x == rect.x) && (y == rect.y) &&
+			(w == rect.w) && (h == rect.h);
 	}
 
 	bool Rect::operator !=(const SDL_Rect &rect)
@@ -120,7 +119,7 @@ namespace SDL
 		return *this;
 	}
 
-	Rect &Rect::operator +(const SDL_Rect &rect)
+	Rect Rect::operator +(const SDL_Rect &rect)
 	{
 		x += rect.x;
 		y += rect.y;
@@ -130,7 +129,7 @@ namespace SDL
 		return *this;
 	}
 
-	Rect &Rect::operator -(const SDL_Rect &rect)
+	Rect Rect::operator -(const SDL_Rect &rect)
 	{
 		x -= rect.x;
 		y -= rect.y;
@@ -143,8 +142,8 @@ namespace SDL
 	// Compare two Rect's
 	bool Rect::operator ==(const Rect &rect)
 	{
-		return (x == rect.x) && (y == rect.y) && (w == rect.w) &&
-			(h == rect.h);
+		return (x == rect.x) && (y == rect.y) &&
+			(w == rect.w) && (h == rect.h);
 	}
 
 	bool Rect::operator !=(const Rect &rect)
@@ -185,7 +184,7 @@ namespace SDL
 		return *this;
 	}
 
-	Rect &Rect::operator +(const Rect &rect)
+	Rect Rect::operator +(const Rect &rect)
 	{
 		x += rect.x;
 		y += rect.y;
@@ -195,7 +194,7 @@ namespace SDL
 		return *this;
 	}
 
-	Rect &Rect::operator -(const Rect &rect)
+	Rect Rect::operator -(const Rect &rect)
 	{
 		x -= rect.x;
 		y -= rect.y;
@@ -243,15 +242,12 @@ namespace SDL
 		return false;
 	}
 
-	Surface::Surface()
+	Surface::Surface() : m_Surface(NULL), m_DeleteSurface(true)
 	{
-		m_Surface = NULL;
-		m_DeleteSurface = true;
 	}
 
-	Surface::Surface(const Surface &copy)
+	Surface::Surface(const Surface &copy) : m_Surface(NULL), m_DeleteSurface(true)
 	{
-		m_DeleteSurface = true;
 		if(copy.m_Surface != NULL)
 		{
 			// get the surface information from this
@@ -264,20 +260,14 @@ namespace SDL
 			throw LogicError("Surface's m_Surface passed to constructor was NULL");
 	}
 
-	Surface::Surface(SDL_Surface *surface)
+	Surface::Surface(SDL_Surface *surface) : m_Surface(surface), m_DeleteSurface(true)
 	{
-		m_DeleteSurface = true;
-		m_Surface = surface;
-
 		if(m_Surface == NULL)
 			throw LogicError("SDL_Surface passed to constructor was NULL");
 	}
 
-	Surface::Surface(int w, int h, int bpp, Uint32 flags)
+	Surface::Surface(int w, int h, int bpp, Uint32 flags) : m_Surface(NULL), m_DeleteSurface(true)
 	{
-		m_DeleteSurface = true;
-		m_Surface = NULL;
-
 		Uint32 Rmask, Gmask, Bmask, Amask;
 
 		/* SDL interprets each pixel as a 32-bit number, so our masks must depend
@@ -298,10 +288,8 @@ namespace SDL
 			throw RuntimeError("Error creating surface with CreateRGB: " + GetError());
 	}
 
-	Surface::Surface(bool deletesurface)
+	Surface::Surface(bool deletesurface) : m_Surface(NULL), m_DeleteSurface(deletesurface)
 	{
-		m_DeleteSurface = deletesurface;
-		m_Surface = NULL;
 	}
 
 	Surface::~Surface()
@@ -851,18 +839,17 @@ namespace SDL
 
 		return true;
 	}
-	Overlay::Overlay()
-	{
-		m_Overlay = NULL;
-	}
-
-	Overlay::Overlay(const Overlay &copy)
+	Overlay::Overlay() : m_Overlay(NULL)
 	{
 	}
 
-	Overlay::Overlay(SDL_Overlay *overlay)
+	Overlay::Overlay(const Overlay &copy) : m_Overlay(NULL)
 	{
-		m_Overlay = overlay;
+		// XXX TODO
+	}
+
+	Overlay::Overlay(SDL_Overlay *overlay) : m_Overlay(overlay)
+	{
 	}
 
 	Overlay::~Overlay()

include/SDL4Cpp/SDL4Cpp_cdrom.h
@@ -75,9 +75,19 @@ namespace SDL
 			 */
 			CD(int drive);
 			/*!
+			 * Copy constructor
+			 */
+			CD(const CD &copy);
+
+			/*!
 			 * Destructor that deals with freeing m_CD
 			 */
 			~CD();
+
+			/*!
+			 * Overloaded equals for copying
+			 */
+			CD &operator =(const CD &copy);
 
 			/*!
 			 * \brief Opens a CD-ROM drive for access.

include/SDL4Cpp/SDL4Cpp_joystick.h
@@ -67,12 +67,22 @@ namespace SDL
 			 */
 			Joystick(int index);
 			/*!
+			 * Copy constructor
+			 */
+			Joystick(const Joystick &copy);
+
+			/*!
 			 * \brief Deconstructor
 			 *
 			 * Frees the current joystick
 			 * \throws SDL::RuntimeError if that joystick isn't freed
 			 */
 			~Joystick(void);
+
+			/*!
+			 * Copy
+			 */
+			Joystick &operator =(const Joystick &copy);
 
 			/*!
 			 * \brief Opens a joystick for use.

include/SDL4Cpp/SDL4Cpp_mouse.h
@@ -68,6 +68,10 @@ namespace SDL
 			 *
 			 */
 			~Cursor();
+			/*!
+			 * Copy Cursor
+			 */
+			Cursor &operator =(const Cursor &copy);
 
 			/*!
 			 * \brief Creates a new mouse cursor.

include/SDL4Cpp/SDL4Cpp_rwops.h
@@ -35,7 +35,10 @@ namespace SDL
 		public:
 			RWops();
 			RWops(const std::string file, const std::string mode);
+			RWops(const RWops &copy);
 			~RWops();
+
+			RWops &operator =(const RWops &copy);
 
 			bool FromFile(const std::string file, const std::string mode);
 

include/SDL4Cpp/SDL4Cpp_video.h
@@ -52,6 +52,8 @@ namespace SDL
 	 * </pre>
 	 * \endhtmlonly
 	 */
+	// XXX
+	// TODO redo this so it doesn't inherit a struct
 	class Rect : public SDL_Rect
 	{
 		public:
@@ -118,7 +120,7 @@ namespace SDL
 			 * \return The Rect that is the addition of all values of the Rect
 			 * and the SDL_Rect.
 			 */
-			Rect &operator +(const SDL_Rect &rect);
+			Rect operator +(const SDL_Rect &rect);
 
 			/*!
 			 * \brief Subtracts an SDL_Rect and Rect.
@@ -126,7 +128,7 @@ namespace SDL
 			 * \return The Rect that is the subtraction of all values of Rect
 			 * from Rect.
 			 */
-			Rect &operator -(const SDL_Rect &rect);
+			Rect operator -(const SDL_Rect &rect);
 
 			/*!
 			 * \brief Comparison between twe Rects.
@@ -171,7 +173,7 @@ namespace SDL
 			 * \return The Rect that is the addition of all values of the Rect
 			 * and rect.
 			 */
-			Rect &operator +(const Rect &rect);
+			Rect operator +(const Rect &rect);
 
 			/*!
 			 * \brief Subtracts a rect from Rect.
@@ -179,7 +181,7 @@ namespace SDL
 			 * \return The Rect that is the subtraction of all values of rect
 			 * from Rect.
 			 */
-			Rect &operator -(const Rect &rect);
+			Rect operator -(const Rect &rect);
 	};
 
 	/*!
@@ -1477,6 +1479,11 @@ namespace SDL
 			 * Documentation not written yet.
 			 */
 			~Overlay();
+
+			/*!
+			 * \brief Create a copy of an Overlay
+			 */
+			Overlay &operator =(const Overlay &copy);
 
 			/*!
 			 * \brief Create an Overlay.

src/CMakeLists.txt
@@ -55,7 +55,7 @@ link_libraries(${SDL_LIBRARY} ${MIXER_LINK} ${IMAGE_LINK})
 add_library(SDL4Cpp SHARED ${SOURCES} ${HEADERS})
 
 # Set the compiler/linker flags
-set_property(TARGET SDL4Cpp APPEND PROPERTY COMPILE_FLAGS "-I${INC} -I${SDL_INCLUDE_DIR} ${MIXER_FLAGS} ${IMAGE_FLAGS}")
+set_property(TARGET SDL4Cpp APPEND PROPERTY COMPILE_FLAGS "-I${INC} -I${SDL_INCLUDE_DIR} ${MIXER_FLAGS} ${IMAGE_FLAGS} -Wall -Weffc++")
 set_property(TARGET SDL4Cpp PROPERTY VERSION "1.2.0")
 
 install(TARGETS SDL4Cpp LIBRARY DESTINATION ${LIB_INSTALL_DIR})

src/SDL4Cpp_cdrom.cpp
@@ -24,13 +24,11 @@ namespace SDL
 		return SDL_CDNumDrives();
 	}
 
-	CD::CD()
+	CD::CD() : m_CD(NULL), m_Drive(-1)
 	{
-		m_CD = NULL;
-		m_Drive = -1;
 	}
 
-	CD::CD(int drive)
+	CD::CD(int drive) : m_CD(NULL), m_Drive(-1)
 	{
 		if(!Open(drive))
 			throw SDL::RuntimeError(GetError());

commit 06bf515adc99948f738e18a98063dbd1746f6d74

Fix Rect's constructor for x,y,w,h
* fixes a regression introduced in commit:
  dc6d813f4398119da27520c078b95cc05a43654a

src/SDL4Cpp_video.cpp
@@ -80,10 +80,10 @@ namespace SDL
 
 	Rect::Rect(int x, int y, int w, int h)
 	{
-		x = x;
-		y = y;
-		w = w;
-		h = h;
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
 	}
 
 	Rect::Rect(const SDL_Rect &rect)